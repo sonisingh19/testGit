8637f1896f32b8a803a6fb3e97a41f3ca11ae2fe
320 63in <320 63in@LAPTOP-G0Q920BM>
2020-07-12 22:13:12 +0530

    This is my first commit code

---
+++ testGitProject/src/testGitProject/Test1.java
@@ -0,0 +1,12 @@
+package testGitProject;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my new project");
+
+	}
+
+}
